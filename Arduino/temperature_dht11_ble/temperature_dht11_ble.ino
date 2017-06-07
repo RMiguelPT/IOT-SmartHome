86b453ca15e98bbb7e9dbd969a3982ee6a1a7a14
Rmiguel <ruben.miguel.55@gmail.com>
2017-06-07 18:13:59 +0100

    Adicionada funcao que verifica se foi acabado de conectar

--- Arduino/temperature_dht11_ble/temperature_dht11_ble.ino
+++ Arduino/temperature_dht11_ble/temperature_dht11_ble.ino
@@ -13,6 +13,18 @@
 #define SENS_SIGNAL 12
 SoftwareSerial ble(5,6);
 
+
+#define MQTT_SENSOR_TOPIC "hassbian"
+#define MQTT_SENSOR_TYPE "sensor"
+#define MQTT_SENSOR_ID "room_temp"
+#define MQTT_SENSOR_CONFIG "config"
+
+#define MQTT_SENSOR_FULL_CONFIG "hassbian/sensor/roomTemp/config"
+
+/*FUNCTION PROTOTYPES*/
+bool is_just_connected();
+void send_config();
+
 //The setup function is called once at startup of the sketch
 void setup()
 {
@@ -24,16 +36,50 @@ void setup()
 
 	Serial.begin(9600);
 	ble.begin(9600);
+
+	ble.setTimeout(200);
 }
 
 // The loop function is called in an endless loop
 void loop()
 {
-	while (ble.available()){
+	/*while (ble.available()){
 		Serial.write(ble.read());
+	}*/
+	if (is_just_connected())
+	{
+		Serial.println("CONNECTED");
+		delay(2000);
+		send_config();
 	}
+
+
 	if (Serial.available()){
 		ble.write(Serial.read());
 	}
 	//Add your repeated code here
+}
+
+bool is_just_connected()
+{
+	if (ble.available()>0)
+	{
+		if (ble.readString()=="OK+CONN")
+		{
+			return true;
+		}
+	}
+	return false;
+}
+
+void send_config()
+{
+	ble.write(MQTT_SENSOR_TOPIC);
+	delay(100);
+	ble.write(MQTT_SENSOR_TYPE);
+	delay(100);
+	ble.write(MQTT_SENSOR_ID);
+	delay(100);
+	ble.write(MQTT_SENSOR_CONFIG);
+	delay(100);
 }
